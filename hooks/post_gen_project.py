d853c17e168d9a930ffd5797f172870ac7a318c6
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-25 20:29:35 -0600

    Install ShellSession when prompted

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,10 +3,15 @@
 from os import getcwd, makedirs
 from cookiecutter.main import cookiecutter
 
+from wotw_blogger.generator.macro import ShellSession
+
 ROOT_DIR = getcwd()
 
 if '{{ cookiecutter.posts_dir }}' != '{{ cookiecutter.templates_dir }}':
     makedirs('{{ cookiecutter.templates_dir }}')
+
+if 'yes' == '{{ cookiecutter.install_shell_session }}':
+    ShellSession().install('{{ cookiecutter.templates_dir }}')
 
 cookiecutter(
     'https://github.com/wizardsoftheweb/wotw-cookiecutter-base.git',
